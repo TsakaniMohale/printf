d01b8a825bfd0a8b94e7e67cd51ad7e6f43b6a54
mr-phosh <mrphosh@gmail.com>
2023-09-19 10:16:21 -0700

    print.c

--- print.c
+++ print.c
@@ -1,44 +1,67 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * print_buffer - Prints the contents of the buffer and resets it
- * @buffer: Array of characters
- * @buff_ind: Pointer to the current index in the buffer
- */
-void print_buffer(char buffer[], int *buff_ind);
-
-/**
- * _printf - Custom printf-like function
+ * _printf - Custom printf function for 'c', 's', and '%'
  * @format: The format string.
- * @...: Variable number of arguments.
  * Return: The number of characters printed.
  */
-int custom_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-    int i, printed_chars = 0;
-    int flags, width, precision, size, buff_ind = 0;
+    int printed_chars = 0;
     va_list args;
-    char buffer[BUFF_SIZE];
-
-    if (format == NULL)
-        return (-1);
 
     va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
+    while (*format)
     {
-        if (format[i] != '%')
+        if (*format != '%')
         {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
+            write(1, format, 1);
             printed_chars++;
         }
         else
         {
-            print_buffer(buffer, &buff_ind);
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, args);
-            precision = get_precision(format, &i, args);
-            size =
+            format++; // Move past '%'
+            if (*format == '\0') // Check for '%' at the end
+                break;
+
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                printed_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    printed_chars++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                printed_chars++;
+            }
+            else
+            {
+                // Handle unsupported format specifier
+                write(1, format - 1, 1);
+                write(1, format, 1);
+                printed_chars += 2;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return printed_chars;
+}
 
