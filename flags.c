3ba094072704b5ec613964962b24bded5a0d8e56
mr-phosh <mrphosh@gmail.com>
2023-09-19 17:50:24 -0700

    adjustments

--- flags.c
+++ flags.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
- * get flags - claculates flags
- * @format: formatted text in which to print the parameters
- * @i : takes on a parameter
- * return: it rerturn flages
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
  */
-
 int get_flags(const char *format, int *i)
 {
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int a, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
 		for (a = 0; FLAGS_CH[j] != '\0'; a++)
 			if (format[curr_i] == FLAGS_CH[a])
 			{
