1ad0f17094a58420c5b580604e46c68afc86e9eb
mr-phosh <mrphosh@gmail.com>
2023-09-19 17:10:29 -0700

    updates

--- utils.c
+++ utils.c
@@ -1,48 +1,40 @@
-include <stdio.h>
-#include <string.h>
-
-#define S_LONG 1
-#define S_SHORT 2
+#include "main.h"
 
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
  *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, 0 if not printable
  */
 int is_printable(char c)
 {
-	if (c >= 32 && c < 127) {
-		return 1;
-	} else {
-		return 0;
-	}
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
 }
 
 /**
- * append_hexa_code - Appends ASCII in hexadecimal code to buffer
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASCII code.
- *
+ * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char *buffer, int i, char ascii_code)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-
 	/* The hexa format code is always 2 digits long */
-	if (ascii_code < 0) {
+	if (ascii_code < 0)
 		ascii_code *= -1;
-	}
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
 	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i++] = map_to[ascii_code % 16];
+	buffer[i] = map_to[ascii_code % 16];
 
-	return 3;
+	return (3);
 }
 
 /**
@@ -53,11 +45,10 @@ int append_hexa_code(char *buffer, int i, char ascii_code)
  */
 int is_digit(char c)
 {
-	if (c >= '0' && c <= '9') {
-		return 1;
-	} else {
-		return 0;
-	}
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
 }
 
 /**
@@ -69,67 +60,28 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
-	switch (size) {
-		case S_LONG:
-			return num;
-		case S_SHORT:
-			return (short)num;
-		default:
-			return (int)num;
-	}
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
 }
 
 /**
  * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
  */
-unsigned long int convert_size_unsgnd(unsigned long int num, int size)
+long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	switch (size) {
-		case S_LONG:
-			return num;
-		case S_SHORT:
-			return (unsigned short)num;
-		default:
-			return (unsigned int)num;
-	}
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
 }
 
-/**
- * print_escaped_string - Prints a string with escape characters
- * @str: String to be printed.
- * @len: Length of the string.
- *
- * Return: None
- */
-void print_escaped_string(char *str, int len)
-{
-	int i;
-
-	for (i = 0; i < len; i++) {
-		if (is_printable(str[i])) {
-			printf("%c", str[i]);
-		} else {
-			append_hexa_code(str, i, str[i]);
-		}
-	}
-}
-
-/**
- * main - Entry point for the program.
- *
- * Return: 0 on success, 1 on failure.
- */
-int main()
-{
-	char str[] = "This is a string with special characters: \n\t!";
-
-	printf("Escaped string: ");
-	print_escaped_string(str, strlen(str));
-	printf("\n");
-
-	return 0;
-}
