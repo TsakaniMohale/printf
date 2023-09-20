4264b2ce6e8d2f6076448f5abc5cf49ba86e7615
mr-phosh <mrphosh@gmail.com>
2023-09-19 18:46:50 -0700

    precison.c

--- precison.c
+++ precison.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
+ * get_precision - Calculates precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * @i: List arguments that will be printed.
+ * @list: list arguments.
  *
  * Return: Precision.
  */
